1b38ef497861ba990a0383e8a2ffd20fa1ea7255
Francisco Nievas <frannievas@gmail.com>
2017-08-03 10:49:21 -0300

    Calibrado eje z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -509,7 +509,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 307, 319, 28571, 603}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 307, 319, 12872, 603}
 
 /**
  * Default Max Feed Rate (mm/s)
